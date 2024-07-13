426bc2d571e3ae79ada27f994c883e94f5791507
Nataly Isaeva <mamadany25@gmail.com>
2024-07-14 00:15:31 +0300

    Домашка испр

--- conftest.py
+++ conftest.py
@@ -3,11 +3,11 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(autouse=true)
+@pytest.fixture(autouse=True)
 def browser_setup():
     browser.config.window_height = 2000
     browser.config.window_width = 1200
-    browser.config.base_url = 'http://google.com'
+    browser.config.base_url = 'https://google.com'
 
     yield
     browser.quit()
